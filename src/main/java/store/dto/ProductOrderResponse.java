bd39be227880d9668566ec7a73ce2d179277cc38
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-12-02 23:59:12 +0900

    feat: ProductOrderResponse 내부 구현 완료

--- src/main/java/store/dto/ProductOrderResponse.java
+++ src/main/java/store/dto/ProductOrderResponse.java
@@ -1,6 +1,10 @@
 package store.dto;
 
 public record ProductOrderResponse(
-
+    boolean executable,
+    int normalInventory,
+    int promotionInventory,
+    int unPromotableInventory,
+    int bonusReceivable
 ) {
 }
