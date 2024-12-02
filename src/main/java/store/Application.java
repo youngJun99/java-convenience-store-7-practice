fc13ecece317cf7e713e154feda7730bf75a0729
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-12-03 03:58:32 +0900

    feat: OrderService 구현 완료

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,10 @@
 package store;
 
+import camp.nextstep.edu.missionutils.DateTimes;
+
+import java.time.LocalDateTime;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
     }
 }
---
+++ src/main/java/store/service/OrderService.java
@@ -0,0 +1,70 @@
+package store.service;
+
+import store.domain.Store;
+import store.dto.*;
+import store.handler.InputHandler;
+import store.view.OutputView;
+
+import java.util.List;
+
+public class OrderService {
+
+    private final InputHandler inputHandler;
+    private final OutputView outputView;
+
+    public OrderService(InputHandler inputHandler, OutputView outputView) {
+        this.inputHandler = inputHandler;
+        this.outputView = outputView;
+    }
+
+    public List<ProductReceipt> processOrder(Store store) {
+        List<ProductInventory> inventories = store.showInventories();
+        outputView.printInventoryStatus(inventories);
+        List<ProductOrderRequest> requests = inputHandler.getOrders();
+        List<ProductOrderResponse> responses = store.requestOrder(requests);
+        List<ConfirmedOrderRequest> confirmedRequests = confirmOrders(responses);
+        return store.executeOrder(confirmedRequests);
+    }
+
+    private List<ConfirmedOrderRequest> confirmOrders(List<ProductOrderResponse> responses) {
+        return responses.stream()
+                .map(response -> {
+                    if (response.executable()) {
+                        return new ConfirmedOrderRequest(
+                                response.productName(),
+                                response.normalInventory(),
+                                response.promotionInventory(),
+                                response.orderedTime()
+                        );
+                    }
+                    if (response.unPromotableInventory() != 0) {
+                        boolean buyUnPromotable = inputHandler.confirmUnPromotableOrders(response);
+                        if (buyUnPromotable) {
+                            return new ConfirmedOrderRequest(
+                                    response.productName(),
+                                    response.unPromotableInventory(),
+                                    response.promotionInventory(),
+                                    response.orderedTime());
+                        }
+                        return new ConfirmedOrderRequest(
+                                response.productName(),
+                                response.normalInventory(),
+                                response.promotionInventory(),
+                                response.orderedTime());
+                    }
+                    boolean getBonuse = inputHandler.confirmBonusReceivableOrders(response);
+                    if (getBonuse) {
+                        return new ConfirmedOrderRequest(
+                                response.productName(),
+                                response.normalInventory(),
+                                response.promotionInventory() + response.bonusReceivable(),
+                                response.orderedTime());
+                    }
+                    return new ConfirmedOrderRequest(
+                            response.productName(),
+                            response.normalInventory(),
+                            response.promotionInventory(),
+                            response.orderedTime());
+                }).toList();
+    }
+}
