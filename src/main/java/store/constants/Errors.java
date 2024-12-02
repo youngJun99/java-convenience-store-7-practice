3ed5cb7c1699102046f27b3704b322a91a886868
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-12-03 03:09:54 +0900

    feat: ErrorCatcher 구현 완료

--- src/main/java/store/constants/Errors.java
+++ src/main/java/store/constants/Errors.java
@@ -9,7 +9,8 @@ public enum Errors {
     ORDER_INPUT_NOT_VALID("올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요"),
     ORDER_OVER_INVENTORY("재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요."),
     NOT_EXISTING_PRODUCT("존재하지 않는 상품입니다. 다시 입력해 주세요."),
-    INPUT_NOT_VALID("잘못된 입력입니다. 다시 입력해 주세요.`");
+    INPUT_NOT_VALID("잘못된 입력입니다. 다시 입력해 주세요.`"),
+    OVER_MAX_RETRIES("재시도 횟수 %d번을 초과했습니다. 시스템을 종료합니다.");
 
 
     private static final String PREFIX = "[ERROR] ";
