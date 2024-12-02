8670e39a64b23e7fc6fff7135b0aca5e18725aa6
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-12-03 00:26:49 +0900

    feat: Promotion 하에서 재고 관리 매서드 구현

--- src/main/java/store/domain/promotion/Promotion.java
+++ src/main/java/store/domain/promotion/Promotion.java
@@ -1,5 +1,8 @@
 package store.domain.promotion;
 
+import store.dto.ProductOrderRequest;
+import store.dto.ProductOrderResponse;
+
 import java.time.LocalDateTime;
 
 public class Promotion {
@@ -22,4 +25,26 @@ public class Promotion {
         return orderedTime.isAfter(start) && orderedTime.isBefore(end);
     }
 
+    public ProductOrderResponse checkPromotableOrder(ProductOrderRequest request, int promotionInventory) {
+        int requested = request.buyAmount();
+        if (requested <= promotionInventory) {
+            if (requested % (buy + get) == 0) {
+                return ProductOrderResponse.executablePromotionOrder(request.productName(), requested);
+            }
+            if (requested % (buy + get) == buy && requested + get <= promotionInventory) {
+                return ProductOrderResponse.bonusReceivableOrder(request.productName(), promotionInventory, get);
+            }
+            int unPromotable = requested % (buy + get);
+            int promotable = requested - unPromotable;
+            return ProductOrderResponse.unPromotableOrder(request.productName(), promotable, unPromotable);
+        }
+
+        if (promotionInventory % (buy + get) == 0) {
+            return ProductOrderResponse.unPromotableOrder(request.productName(), promotionInventory, requested - promotionInventory);
+        }
+        int promotable = (buy + get) * (int) (promotionInventory / (buy + get));
+        return ProductOrderResponse.unPromotableOrder(request.productName(), promotable, requested - promotable);
+    }
+
 }
+
--- src/main/java/store/dto/ProductOrderResponse.java
+++ src/main/java/store/dto/ProductOrderResponse.java
@@ -1,25 +1,26 @@
 package store.dto;
 
 public record ProductOrderResponse(
+        String productName,
         boolean executable,
         int normalInventory,
         int promotionInventory,
         int unPromotableInventory,
         int bonusReceivable
 ) {
-    public static ProductOrderResponse executableNormalOrder(int normalInventory) {
-        return new ProductOrderResponse(true, normalInventory, 0, 0, 0);
+    public static ProductOrderResponse executableNormalOrder(String productName, int normalInventory) {
+        return new ProductOrderResponse(productName, true, normalInventory, 0, 0, 0);
     }
 
-    public static ProductOrderResponse executablePromotionOrder(int promotionInventory) {
-        return new ProductOrderResponse(true, 0, promotionInventory, 0, 0);
+    public static ProductOrderResponse executablePromotionOrder(String productName, int promotionInventory) {
+        return new ProductOrderResponse(productName, true, 0, promotionInventory, 0, 0);
     }
 
-    public static ProductOrderResponse bonusReceivableOrder(int promotionInventory, int bonusReceivable) {
-        return new ProductOrderResponse(false, 0, promotionInventory, 0, bonusReceivable);
+    public static ProductOrderResponse bonusReceivableOrder(String productName, int promotionInventory, int bonusReceivable) {
+        return new ProductOrderResponse(productName, false, 0, promotionInventory, 0, bonusReceivable);
     }
 
-    public static ProductOrderResponse unPromotableOrder(int promotionInventory, int unPromotableInventory) {
-        return new ProductOrderResponse(true, 0, promotionInventory, unPromotableInventory, 0);
+    public static ProductOrderResponse unPromotableOrder(String productName, int promotionInventory, int unPromotableInventory) {
+        return new ProductOrderResponse(productName, true, 0, promotionInventory, unPromotableInventory, 0);
     }
 }
