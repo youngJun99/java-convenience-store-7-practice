6fc410c760a78f315a7de82bbb2230986dea2844
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-12-03 03:02:31 +0900

    feat: StoreReader과 PromotionReader 구현 완료

---
+++ src/main/java/store/reader/PromotionReader.java
@@ -0,0 +1,35 @@
+package store.reader;
+
+import store.domain.promotion.Promotion;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PromotionReader{
+
+    public List<Promotion> createPromotions(String promotionPath) throws IOException {
+        List<Promotion> promotions = new ArrayList<>();
+        Path path = Paths.get(promotionPath);
+        List<String> lines = Files.readAllLines(path);
+        for (int i = 1; i < lines.size(); i++) {
+            extractPromotions(lines.get(i), promotions);
+        }
+        return promotions;
+    }
+
+    private void extractPromotions(String line, List<Promotion> promotions) {
+        String[] data = line.split(",");
+        String name = data[0].trim();
+        int buy = Integer.parseInt(data[1].trim());
+        int get = Integer.parseInt(data[2].trim());
+        LocalDateTime startDate = LocalDateTime.parse(data[3].trim());
+        LocalDateTime endDate = LocalDateTime.parse(data[4].trim());
+
+        promotions.add(new Promotion(name,startDate,endDate, buy, get));
+    }
+}
---
+++ src/main/java/store/reader/StoreReader.java
@@ -0,0 +1,49 @@
+package store.reader;
+
+import store.domain.Product;
+import store.domain.Store;
+import store.domain.promotion.Promotion;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class StoreReader {
+
+    public Store createStore(List<Promotion> promotions, String productsPath) throws IOException {
+        List<Product> products = new ArrayList<>();
+        Path path = Paths.get(productsPath);
+        List<String> lines = Files.readAllLines(path);
+        for (int i = 1; i < lines.size(); i++) {
+            String[] data = lines.get(i).split(",");
+            String productName = data[0].trim();
+            int price = Integer.parseInt(data[1].trim());
+            boolean isAlreadyProcessed = products.stream()
+                    .filter(product -> product.getName().equals(productName))
+                    .findAny()
+                    .map(product -> {
+                        product.refillNormalQuantity(Integer.parseInt(data[2].trim()));
+                        return true;
+                    })
+                    .orElse(false);
+            if (isAlreadyProcessed) {
+                continue;
+            }
+            String promotionName = data[3].trim();
+            if (promotionName.equals("null")) {
+                products.add(new Product(productName, price, null, Integer.parseInt(data[2].trim()), 0));
+                continue;
+            }
+            Optional<Promotion> promotion = promotions.stream()
+                    .filter(promotionSelected -> promotionSelected.getName().equals(productName))
+                    .findFirst();
+            products.add(new Product(productName, price, promotion, 0, Integer.parseInt(data[2].trim())));
+        }
+        return new Store(products);
+    }
+
+}
