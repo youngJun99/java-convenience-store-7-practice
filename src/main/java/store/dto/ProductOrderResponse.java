e7f5084eaf871584e189ab364b9a828365cd49dc
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-12-03 00:07:02 +0900

    feat: ProductOrderResponse 상황에 따른 펙토리 매서드 구현

--- src/main/java/store/dto/ProductOrderResponse.java
+++ src/main/java/store/dto/ProductOrderResponse.java
@@ -1,10 +1,25 @@
 package store.dto;
 
 public record ProductOrderResponse(
-    boolean executable,
-    int normalInventory,
-    int promotionInventory,
-    int unPromotableInventory,
-    int bonusReceivable
+        boolean executable,
+        int normalInventory,
+        int promotionInventory,
+        int unPromotableInventory,
+        int bonusReceivable
 ) {
+    public static ProductOrderResponse executableNormalOrder(int normalInventory) {
+        return new ProductOrderResponse(true, normalInventory, 0, 0, 0);
+    }
+
+    public static ProductOrderResponse executablePromotionOrder(int promotionInventory) {
+        return new ProductOrderResponse(true, 0, promotionInventory, 0, 0);
+    }
+
+    public static ProductOrderResponse bonusReceivableOrder(int promotionInventory, int bonusReceivable) {
+        return new ProductOrderResponse(false, 0, promotionInventory, 0, bonusReceivable);
+    }
+
+    public static ProductOrderResponse unPromotableOrder(int promotionInventory, int unPromotableInventory) {
+        return new ProductOrderResponse(true, 0, promotionInventory, unPromotableInventory, 0);
+    }
 }
